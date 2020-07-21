ad8d0531b07047f313295c2e7b5de051ec5a82f3
Payton Swick <payton@foolord.com>
2020-07-21 17:39:43 -0400

    Add useShoppingCartManager hook

---
+++ client/my-sites/checkout/composite-checkout/wpcom/hooks/use-shopping-cart-manager/use-show-add-coupon-success-message.ts
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { useEffect } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { ResponseCart } from '../../types';
+
+export default function useShowAddCouponSuccessMessage(
+	didAddCoupon: boolean,
+	responseCart: ResponseCart,
+	showAddCouponSuccessMessage: ( arg0: string ) => void,
+	hookDispatch: ( arg0: ShoppingCartHookAction ) => void
+): void {
+	useEffect( () => {
+		if ( didAddCoupon ) {
+			showAddCouponSuccessMessage( responseCart.coupon );
+			hookDispatch( { type: 'DID_SHOW_ADD_COUPON_SUCCESS_MESSAGE' } );
+		}
+	}, [ didAddCoupon, responseCart.coupon, showAddCouponSuccessMessage, hookDispatch ] );
+}
